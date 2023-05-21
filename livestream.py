7d2cc0e9fa7a8ebd2d1367372955e6597a6ff469
Colin Casey <colincas37@gmail.com>
2023-05-20 21:43:59 -0700

    deleted timelapse stuff from livestream.py + started working on the camera project again may 20th

--- livestream.py
+++ livestream.py
@@ -2,7 +2,6 @@ import subprocess
 import picamera2
 import time
 import signal
-import boto3
 from datetime import datetime
 import threading
 
@@ -15,21 +14,12 @@ WIDTH = 1280
 HEIGHT = 720
 FRAMERATE = 30
 BITRATE = 2000  # kbps
-# LIVESTREAM_DURATION = 60 # time in seconds 
 
-# Timelapse settings
-TIMELAPSE_INTERVAL = 60  # seconds between images
-TIMELAPSE_DURATION = 3600  # total duration of the timelapse in seconds
-
-# AWS S3 settings
-S3_BUCKET = "your-s3-bucket-name"
 
 # Create a command for ffmpeg to stream to YouTube
 command = (
     f"ffmpeg -f h264 -i - -f lavfi -i anullsrc -c:v copy -c:a aac -f flv rtmp://a.rtmp.youtube.com/live2/{YOUTUBE_STREAM_KEY}"
 )
-
-
 
 def start_ffmpeg():
     """Start the ffmpeg subprocess."""
@@ -64,56 +54,8 @@ def livestream(duration):
     stop_ffmpeg(ffmpeg_process)
 
 
-# def capture_timelapse_image():
-#     """Capture a timelapse image and return the image data."""
-#     with picamera2.PiCamera(resolution=(WIDTH, HEIGHT)) as camera:
-#         time.sleep(2)  # Allow the camera to warm up
-#         image_data = camera.capture(output_format="jpeg")
-#     return image_data
-
-
-# def upload_to_s3(image_data, s3_key):
-#     """Upload the given image data to the specified S3 key."""
-#     s3 = boto3.client("s3")
-#     s3.put_object(Bucket=S3_BUCKET, Key=s3_key, Body=image_data, ContentType="image/jpeg")
-
-
-# def timelapse():
-#     """Capture timelapse images and upload them to S3."""
-#     start_time = time.time()
-#     while time.time() - start_time < TIMELAPSE_DURATION:
-#         # Capture a timelapse image
-#         image_data = capture_timelapse_image()
-
-#         # Generate a unique key for the image in S3
-#         timestamp = datetime.utcnow().strftime("%Y-%m-%d_%H-%M-%S")
-#         s3_key = f"timelapse/{timestamp}.jpg"
-
-#         # Upload the image to S3
-#         upload_to_s3(image_data, s3_key)
-
-#         # Wait for the next timelapse interval
-#         time.sleep(TIMELAPSE_INTERVAL)
-
-
-
 if __name__ == "__main__":
     # Stream for 60 seconds as an example
     livestream_duration = 60
     livestream(livestream_duration)
 
-    # Run the timelapse
-    # timelapse()
-
-# def livestream_and_timelapse():
-#     livestream_thread = threading.Thread(target=livestream, args=(LIVESTREAM_DURATION,))
-#     timelapse_thread = threading.Thread(target=timelapse)
-
-#     livestream_thread.start()
-#     timelapse_thread.start()
-
-#     livestream_thread.join()
-#     timelapse_thread.join()
-
-# if __name__ == "__main__":
-#     livestream_and_timelapse()
